9db2a5851e84e26f3f4383dfd715d9fc43aa56be
Mark A Guza <guzamaguza@gmail.com>
2020-05-29 05:30:11 +0000

    update

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -1,4 +1,15 @@
 class Stadium < ActiveRecord::Base
   belongs_to  :visits
+  @@all = []
 
+  def self.initialize(name, location, team)
+    @name = name
+    @location = location
+    @team = team
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
