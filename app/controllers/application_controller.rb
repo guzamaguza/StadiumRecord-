59aa49c2293b78f2f0aa354081a7946b52dd4a04
Mark A Guza <guzamaguza@gmail.com>
2020-05-30 07:25:48 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+#require_relative '../db/list_of_stadiums'
 
 class ApplicationController < Sinatra::Base
 
@@ -10,9 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    @stadium_hash.each do |key|
-      Stadium.new(key, key.value[0], key.value[1])
-    end
+    #stadium_hash.each do |key|
+    #  Stadium.new(key, key.value[0], key.value[1])
+    #end
 
     erb :welcome
   end
