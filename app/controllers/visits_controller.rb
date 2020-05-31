1a11733a0fc54864585142d047dbef60a374642d
Mark A Guza <guzamaguza@gmail.com>
2020-05-31 05:47:40 +0000

    update

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -8,4 +8,16 @@ class VisitsController < ApplicationController
       end
     end
 
+    get '/visits/new' do
+
+      erb :'/visits/'
+    end
+
+    post '/visits/new' do
+      #get visit stadium, date, user_id and make a new visit
+      @visit = Visit.create(params)
+
+      erb :'/visits'
+    end
+
 end
